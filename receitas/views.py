2c30477904dbb153a9d465c8ecd8c61b1598355b
Eduardo Shoiti <shoitiaragaki@gmail.com>
2022-01-21 18:29:02 -0300

    Curso e projeto finalizado

--- receitas/views.py
+++ receitas/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
+from .models import Receita
 
 def index(request):
-    return render(request, 'index.html')
 
-def receita(request):
-    return render(request, 'receita.html')
+    receitas = Receita.objects.all()
+
+    dados = {
+        'receitas': receitas
+    }
+
+    return render(request, 'index.html', dados)
+
+def receita(request, receita_id):
+    receita = get_object_or_404(Receita, pk=receita_id)
+
+    receita_a_exibir = {
+        'receita': receita
+    }
+
+    return render(request, 'receita.html', receita_a_exibir)
